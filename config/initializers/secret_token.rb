5e4afd125f95fac9a2fda31e67ad405635b1a04a
Ankur Goel <ankurgel@gmail.com>
2012-09-22 09:22:43 +0530

    added basic user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linklog::Application.config.secret_token = 'ff1c56585892c92f334de30822f0078d448a3c04743e5d8a087332981fb9420b873714e920a49af6131054973bf30e6add58731c6a4940f4324834bc6725ae22'
