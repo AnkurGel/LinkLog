84ef987cc0b477cea6f980bdc01c4c2c14f0d4bd
Ankur Goel <ankurgel@gmail.com>
2012-09-24 23:57:26 +0530

    Added functionality to autogenerate the title from page. Appropriate OpenURI exception pending

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
+  require 'nokogiri'
+  require 'open-uri'
   attr_accessible :link, :title
   belongs_to :user
+  before_save :create_title
 
   LINK_FORMAT = /https?:\/\/\S+/
   validates :user_id, :presence => true
@@ -9,4 +12,12 @@ class Post < ActiveRecord::Base
   validates :title, :uniqueness => true
 
   default_scope :order => 'posts.created_at DESC'
+
+  private
+  def create_title
+    if self.link
+      doc = Nokogiri::HTML(open(self.link))
+      doc.search('title').each{ |t| self.title = t.content }
+    end
+  end
 end
