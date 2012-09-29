f1e5f0c7fa0f192442d83d002f7243311ca6092a
Ankur Goel <ankurgel@gmail.com>
2012-09-29 13:25:38 +0530

    Added support to mark post as important and highlighted important links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120926084404) do
+ActiveRecord::Schema.define(:version => 20120929073303) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120926084404) do
     t.datetime "updated_at",                    :null => false
     t.integer  "user_id"
     t.boolean  "private",    :default => false
+    t.boolean  "important",  :default => false
   end
 
   create_table "users", :force => true do |t|
