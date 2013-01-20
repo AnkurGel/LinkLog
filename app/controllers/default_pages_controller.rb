3083079ac0c2cdf0f91928421c5ca6fa741e5dfc
Ankur Goel <ankurgel@gmail.com>
2013-01-20 23:15:34 +0530

    Added alerts wrt search results

--- app/controllers/default_pages_controller.rb
+++ app/controllers/default_pages_controller.rb
@@ -4,6 +4,10 @@ class DefaultPagesController < ApplicationController
       @post = current_user.posts.build
     end
     @logged_links = User.log(params[:search]).paginate(:page => params[:page], :per_page => 8)
+    flash.now[:error] = "No entry in your log matches \"#{params[:search]}\"" if @logged_links.empty?
+    if params[:search] and not params[:search].empty? and not @logged_links.empty?
+      flash.now[:success] = "Showing #{@logged_links.length} results for \"#{params[:search]}\"."
+    end
   end
 
   def about
